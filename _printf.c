e6a42daa14df8d9efe2c053bf01f34b7d3dcc639
EstebanArchbold <est.archbold.edas@gmail.com>
2020-10-27 05:04:08 +0000

    adding 2 functions

--- _printf.c
+++ _printf.c
@@ -1,9 +1,30 @@
 #include "holberton.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * prtChar - print characters
+ * @list: arguments
+ * Return: how many characters has been printed
  */
-testing
+int prtChar(va_list list)
+{
+	put_char(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * print_string - Print a string
+ * @list: arguments
+ * Return: how many characters has been printed
+ */
+int print_string(va_list list)
+{
+	int a;
+	char *string;
+
+	string = va_arg(list, char *);
+	if (string == NULL)
+		string = "(null)";
+	for (a = 0; string[a] != '\0'; i++)
+		put_char(string[a]);
+	return (a);
+}
