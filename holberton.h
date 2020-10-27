432fc6b2cc60cbfb0005fc4f4013b9db56a4ce40
AndresSern <andrecamposer@gmail.com>
2020-10-27 03:49:16 +0000

    New files

--- holberton.h
+++ holberton.h
@@ -1,10 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-void free_grid(int **grid, int height);
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-int **alloc_grid(int width, int height);
 
-#endi|f
+
+typedef struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+}formatos;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
