f413a2c8dc122f1205b61281f80103928472dc87
AndresSern <andrecamposer@gmail.com>
2020-10-27 10:12:36 +0000

    Changed

---
+++ _printf.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+
+/**
+ * parametros - Parameters for printf
+ * @f_list: list of arguments
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	int chars;
+	va_list list;
+	
+	va_start (list, format);
+	if (format == NULL)
+		return (-1);
+
+	chars = charsFormats(format, list);
+
+	va_end(list);
+	return chars;
+}
+int charsFormats(const char *format, va_list args)
+{
+	int a, b, chars, r_val;
+
+	fmtsSpefier f_list[] = {{"c", _char}, {"s", _string}, {"%", _percent}, {NULL, NULL}
+	};
+	chars = 0;
+	for (a = 0; format[a] != '\0'; a++)
+	{
+		if (format[a] == '%')
+		{
+			for (b = 0; f_list[b].sym != NULL; b++)
+			{
+				if (format[a + 1] == f_list[b].sym[0])
+				{
+					r_val = f_list[b].f(args);
+					if (r_val == -1)
+						return (-1);
+					chars += r_val;
+					break;
+				}
+			}
+			if (f_list[b].sym == NULL && format[a + 1] != ' ')
+			{
+				if (format[a + 1] != '\0')
+				{
+					_putchar(format[a]);
+					_putchar(format[a + 1]);
+					chars = chars + 2;
+ 				}
+				else
+					return (-1);
+			}
+		a += 1;
+		}
+		else
+		{
+			_putchar(format[a]);
+			chars++;
+		}
+	}
+	return (chars);
+}
