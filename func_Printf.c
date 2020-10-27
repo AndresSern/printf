8151dc1b52dc0b9f9493f70f09c3da93812d5446
AndresSern <andrecamposer@gmail.com>
2020-10-27 07:07:40 +0000

    Changed

--- func_Printf.c
+++ func_Printf.c
@@ -4,22 +4,47 @@
  * @f_list: list of arguments
  *
  */
-
-void parametros(void)
+int parser(const char *format, va_list arg_list)
 {
-	conver_t f_list[] = {
-	{"c", _char},
-	{"s", _string},
-	{"%", _percent},
-	{"d", _integer},
-	{"i", _integer},
-	{"b", _binary},
-	{"r", _reversed},
-	{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
+	int a, b, c, chars, r_val;
+
+	conver_t f_list[] = {{"c", _char}, {"s", _string}, {"%", _percent},
+	{NULL, NULL}
 	};
+	for (a = 0; format[a]; a++)
+	{
+		if (format[a] == '%')
+		{
+			for (b = 0; f_list[b].sym != NULL; b++)
+			{
+				if (format[a + 1] == f_list[b].sym[0])
+				{
+					r_val = f_list[b].f(arg_list);
+					if (r_val == -1)
+						return (-1);
+					chars += r_val;
+					break;
+				}
+			}
+			if (f_list[b].sym == NULL && format[a + 1] != ' ')
+			{
+				if (format[a + 1] != '\0')
+				{
+					put_char(format[a]);
+					put_char(format[a + 1]);
+					chars += 2;
+ 				}
+				else
+					return (-1);
+			}
+		a += 1;
+		}
+		else
+{
+			chars(format[a]); /*call the write function*/
+			chars++;
+		}
+	}
+	return (chars);
 }
+
